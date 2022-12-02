a223c9efb38fd5da93d08a765eff606fcb07f8d5
Doğukan Kardaş <>
2022-12-02 17:57:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the name generator.")
+word1 = input("Enter the first word you like.\n")
+word2 = input("Enter the second word you like.\n")
+print("Your nane could be " + word1 + "" + word2)
